b1be26b98cafe9d5a73e6262bd2010a4ef5b6ee9
Alex Zappa <alex@zappa.dev>
2023-08-07 03:38:22 +0200

    update public assets and robots file

--- .eleventy.js
+++ .eleventy.js
@@ -20,7 +20,7 @@ module.exports = function (eleventyConfig) {
 	// Copy the contents of the `public` folder to the output folder
 	// For example, `./public/css/` ends up in `_site/css/`
 	eleventyConfig.addPassthroughCopy({
-		"./public/": "/",
+		"./src/public/": "/",
 		"./src/content/**/*.mp4": "/videos/",
 	});
 
