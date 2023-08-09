e91564ec96fc2e7192ffb52e75e7f8b91d456465
Alex Zappa <alex@zappa.dev>
2023-08-09 15:48:48 +0200

    WIP init MetaGen plugin

--- src/_data/metadata.js
+++ src/_data/metadata.js
@@ -1,11 +1,16 @@
 module.exports = {
-	"title": "Alex Zappa",
-	"url": "https://alex.zappa.dev/",
-	"language": "en",
-	"description": "I am writing about my experiences as a naval navel-gazer.",
-	"author": {
-		"name": "Alex Zappa",
-		"email": "alex@zappa.dev",
-		"url": "https://alex.zappa.dev/about-me/"
-	}
-}
+    title: "Alex Zappa",
+    url: "https://alex.zappa.dev",
+    language: "en",
+    description: "I am writing about my experiences as a naval navel-gazer.",
+    openGraph: {
+        image: "/default-og-image.png",
+        imageAlt: "",
+    },
+    author: {
+        name: "Alex Zappa",
+        twitter: "reatlat",
+        email: "alex@zappa.dev",
+        url: "https://alex.zappa.dev/about/",
+    },
+};
