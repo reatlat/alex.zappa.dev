fb13b8eca38fa0a6383bbe2fcab3f6c7dfebc51b
Alex Zappa <alex@zappa.dev>
2023-08-12 19:04:01 +0200

    update hello world

--- api/hello.js
+++ api/hello.js
@@ -1,10 +1,14 @@
 // Hello World API
 
 exports.handler = async (event, context) => {
+	console.log({ event }, { context });
+
+	const name = (event.queryStringParameters || {}).name || "World";
+
 	return {
 		statusCode: 200,
 		body: JSON.stringify({
-			message: "Hello World",
+			message: `Hello ${name}!`,
 		}),
 	};
 };
