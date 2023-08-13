17de68c48c5d160e077f4af9843314d38d24978e
Alex Zappa <alex@zappa.dev>
2023-08-13 05:12:36 +0200

    fix script to create new posts

--- scripts/new-post.mjs
+++ scripts/new-post.mjs
@@ -8,30 +8,31 @@ import slugify from "slugify";
 // Ask the name of the new post and create it
 
 const questions = [
-	{
-		type: "input",
-		name: "title",
-		message: "What is the title of the new post?",
-	},
+  {
+    type: "input",
+    name: "title",
+    message: "What is the title of the new post?",
+  },
 ];
 
 inquirer.prompt(questions).then((answers) => {
-	const title = answers.title;
-	const slug = slugify(title, {
-		remove: /[_*+~.()'"!:@]/g,
-		strict: true,
-		lower: true,
-	});
-	const date = DateTime.local().toFormat("yyyy-LL-dd");
-	const year = DateTime.local().toFormat("yyyy");
-	const filepath = `./src/content/blog/${year}/${slug}/index.md`;
+  const title = answers.title;
+  const slug = slugify(title, {
+    remove: /[_*+~.()'"!:@]/g,
+    strict: true,
+    lower: true,
+  });
+  const date = DateTime.local().toFormat("yyyy-LL-dd");
+  const year = DateTime.local().toFormat("yyyy");
+  const filepath = `./src/content/blog/${year}/${slug}/index.md`;
 
-	// Create the directory and file
-	mkdirSync(path.dirname(filepath), { recursive: true });
-	writeFileSync(
-		filepath,
-		`---
-title: ${title}
+  // Create the directory and file
+  mkdirSync(path.dirname(filepath), { recursive: true });
+  writeFileSync(
+    filepath,
+    `---
+title: "${title}"
+desc: ""
 date: ${date}
 tags: []
 draft: true
@@ -39,7 +40,7 @@ draft: true
 This is a draft post
 
 `
-	);
+  );
 
-	console.log(chalk.green(`Created ${filepath}`));
+  console.log(chalk.green(`Created ${filepath}`));
 });
