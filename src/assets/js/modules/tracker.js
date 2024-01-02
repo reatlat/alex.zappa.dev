5b74a4e561245f67e055d2e09bbd321c629dcf16
Alex Zappa <alex@zappa.dev>
2024-01-02 10:10:30 +0100

    Updated apiHost and simplified 404 event tracking.
    The apiHost used by the Plausible script was changed from "https://alex.zappa.dev" to "https://plausible.zappa.dev" in tracker.js. Additionally, the event tracking for 404 errors was simplified by removing an unnecessary nested 'props' property, resulting in a less complex data structure.

--- src/assets/js/modules/tracker.js
+++ src/assets/js/modules/tracker.js
@@ -4,7 +4,7 @@ import colorScheme from "./_detectColorScheme";
 
 const { trackEvent, trackPageview, enableAutoOutboundTracking } = Plausible({
     domain: "alex.zappa.dev",
-    apiHost: "https://alex.zappa.dev",
+    apiHost: "https://plausible.zappa.dev",
 });
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -83,15 +83,13 @@ document.addEventListener("DOMContentLoaded", () => {
     if (document.body.classList.contains("page-404")) {
         trackEvent("404", {
             props: {
-                props: {
-                    title: document.title,
-                    url: location.href,
-                    path: location.pathname,
-                    referrer: document.referrer,
-                    prefersColorScheme: colorScheme(),
-                    userAgent: navigator.userAgent,
-                    deviceWidth: window.innerWidth,
-                },
+                title: document.title,
+                url: location.href,
+                path: location.pathname,
+                referrer: document.referrer,
+                prefersColorScheme: colorScheme(),
+                userAgent: navigator.userAgent,
+                deviceWidth: window.innerWidth,
             },
         });
 
