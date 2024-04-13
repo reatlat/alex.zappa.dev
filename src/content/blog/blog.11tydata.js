ec0f6959939580342af9f1469f8f232b42584c28
Alex Zappa <alex@zappa.dev>
2024-04-14 00:10:32 +0200

    Correct syntax errors in module exports
    Corrected syntax errors in the module exports of the 'feed' and 'blog' data files. The changes include adding missing trailing commas and semicolons to ensure proper function execution.

--- src/content/blog/blog.11tydata.js
+++ src/content/blog/blog.11tydata.js
@@ -1,5 +1,5 @@
 module.exports = {
-	tags: ["posts"],
-	layout: "post.njk",
-	permalink: "/blog/{{ title | slugify }}/",
+    tags: ["posts"],
+    layout: "post.njk",
+    permalink: "/blog/{{ title | slugify }}/",
 };
