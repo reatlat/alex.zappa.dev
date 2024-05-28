d2457b1ab0d9687d001a6fdad85aade466ca7a38
Alex Zappa <alex@zappa.dev>
2024-05-28 06:13:49 +0200

    Add UUID generation function
    A UUID (Universally Unique Identifier) generation function was added to the Alpine.data/DOM.js module. This function generates a UUID by replacing 'x' characters in the standard UUID format with random hex digits, and 'y' with 8, 9, A or B.

--- src/assets/js/modules/Alpine.data/DOM.js
+++ src/assets/js/modules/Alpine.data/DOM.js
@@ -41,6 +41,17 @@ export default () => {
             );
         },
 
+        uuid() {
+            return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(
+                /[xy]/g,
+                (c) => {
+                    const r = (Math.random() * 16) | 0;
+                    const v = c === "x" ? r : (r & 0x3) | 0x8;
+                    return v.toString(16);
+                },
+            );
+        },
+
         init() {
             debugLog("AlpineJS DOM init");
             document.documentElement.classList.remove("dark");
